308855b402da988bcdead8941eeb3e37ec8b08db
Zhang Hongchao <hozhang@microstrategy.com>
2024-06-04 23:59:38 +0800

    self run and use azure openai

--- backend/core.py
+++ backend/core.py
@@ -1,27 +1,44 @@
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings, AzureOpenAIEmbeddings, AzureChatOpenAI
 
 from typing import Any, Dict, List
 from langchain.chains import ConversationalRetrievalChain
 from langchain_pinecone import PineconeVectorStore
 
-
-INDEX_NAME = "langchain-doc-index"
+INDEX_NAME = "langchain-concepts"
 
 
 def run_llm(query: str, chat_history: List[Dict[str, Any]] = []):
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    # embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+    embeddings = AzureOpenAIEmbeddings(model="text-embedding-3-small",
+                                       openai_api_key=os.getenv("AZURE_OPENAI_API_KEY"),
+                                       azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"),
+                                       openai_api_version=os.getenv("OPENAI_API_VERSION"))
     docsearch = PineconeVectorStore(embedding=embeddings, index_name=INDEX_NAME)
 
-    chat = ChatOpenAI(
+    # for using openai directly
+    # chat = ChatOpenAI(
+    #     verbose=True,
+    #     temperature=0,
+    # )
+
+    # for using azure openai
+    chat = AzureChatOpenAI(
         verbose=True,
         temperature=0,
+        deployment_name="gpt-35-turbo-16k",
     )
 
     qa = ConversationalRetrievalChain.from_llm(
         llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
     )
     return qa.invoke({"question": query, "chat_history": chat_history})
+
+
+if __name__ == "__main__":
+    answer = run_llm("Describe AgentExecutorIterator?")
+    print(answer)
